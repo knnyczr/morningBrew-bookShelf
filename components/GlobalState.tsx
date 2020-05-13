5b8d97d43added343ea547f688bd0584ba368048
KennyCruzer <kennycruz@Kenneths-MacBook-Pro.local>
2020-05-12 21:16:47 -0400

    successful googleapi

--- components/GlobalState.tsx
+++ components/GlobalState.tsx
@@ -1,7 +1,9 @@
 import React, { createContext } from 'react';
 
+
 export const initialValues = {
-    books: [1,2,3]
+    books: [1,2,3], 
+    // sheetData: []
 }
 
 export const GlobalContext = createContext(initialValues)
@@ -9,9 +11,17 @@ export const GlobalContext = createContext(initialValues)
 export const GlobalProvider: React.FC = ({children}) => {
   return (
     <GlobalContext.Provider value={{
-        books: []
+        books: [], 
+        // sheetData: []
     }}>
         {children}
     </GlobalContext.Provider>
   );
 }
+
+// export const getStaticProps: GetStaticProps = async() => {
+//     const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
+//     const posts = await res.json()
+
+//     return {props: { posts }}
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,24 @@
 import { useContext } from 'react'
 import Head from 'next/head';
 import NavBarComponent from '../components/NavBarComponent';
-import { Button } from 'react-bootstrap'
 import { GlobalProvider, initialValues } from '../components/GlobalState'
 import { GlobalContext } from '../components/GlobalState'
+import { GetStaticProps } from 'next'
+import fetch from 'isomorphic-unfetch'
+
+// import { google } from 'googleapis'
 
 
-export default function Home() {
+export default function Home(props) {
+  const { sheetData } = props
   const { books } = useContext(GlobalContext)
-  console.log(books)
+  console.log(books, sheetData.values)
   return (
     <GlobalProvider>
       <div className="container">
         <Head>
           <title>Morning Brew | bookShelf</title>
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" ></link>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
         </Head>
         <main>
           <NavBarComponent />
@@ -23,4 +27,18 @@ export default function Home() {
       </div>
     </GlobalProvider>
   )
+}
+
+export const getStaticProps: GetStaticProps = async() => {
+  // const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
+  const res = await fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/${
+        process.env.googleSheetsID
+      }/values/${process.env.range}?key=${
+        process.env.apiKey
+      }`
+  )
+  const sheetData = await res.json()
+
+  return {props: { sheetData }}
 }
