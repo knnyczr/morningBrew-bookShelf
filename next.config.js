ee6b3d1f12c96d9f33a80b1c66e9d3137d145928
Kenneth Cruz <37004078+knnyczr@users.noreply.github.com>
2020-05-17 00:13:27 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    env: {
-        googleSheetsID: '1N_BrwBbclkG-pxVNm-wN6-t0CDfDA7MQb8RZ1YVVvxk',
-        range: 'April!A1:H14', 
-        apiKey: 'AIzaSyDWN7cBbUFnArZec43LWgyQObFDw5l-5vA'
-    },
-}
